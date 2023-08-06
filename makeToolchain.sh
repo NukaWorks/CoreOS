7adc5f36ad92e662653dc1dce17b8c38be7163f4
powerm1nt <nukaworks@nuka.works>
2023-08-06 20:14:25 +0200

    makeToolchain: Absolute directory

--- makeToolchain.sh
+++ makeToolchain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p toolchain/
-export TOOLCHAIN_ROOT=./toolchain/
+export TOOLCHAIN_ROOT=$(pwd)/toolchain/
 export LFS_TGT=$(uname -m)-lfs-linux-gnu
 
 echo "Building binutils (1/2)..."
