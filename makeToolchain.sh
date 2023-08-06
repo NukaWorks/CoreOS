51ba99e0a2ca25f5756c3c37c3c6d179f06a125f
powerm1nt <nukaworks@nuka.works>
2023-08-06 19:53:47 +0200

    Test

--- makeToolchain.sh
+++ makeToolchain.sh
@@ -7,9 +7,9 @@ echo "Building binutils (1/2)..."
 cd binutils
 mkdir -p build && cd build
 
-../configure --prefix=$TOOLCHAIN_ROOT --target=$LFS_TGT --disable-nls --disable-werror && make && make install
+../configure --prefix=$TOOLCHAIN_ROOT --target=$LFS_TGT --disable-nls --disable-werror && make -j$(nproc) && make install
 
-cd ../.
+cd ../../
 
 echo "Building GCC (1/2)..."
 cp -rvf mpfr gmp mpc gcc/
@@ -43,5 +43,48 @@ mkdir -p build && cd build
   --enable-default-ssp \
   --enable-languages=c,c++ \
   --without-headers &&
-  make && make install
+  make -j$(nproc) && make install
+
+cd ../../
+
+echo "Building Linux Headers ..."
+cd linux
+make mrproper
+make headers -j$(nproc)
+find usr/include -type f ! -name '*.h' -delete
+cp -rv usr/include $TOOLCHAIN_ROOT/usr
+
+cd ../
+echo "building glibc (1/2)..."
+
+cd glibc/
+case $(uname -m) in
+    i?86)   ln -sfv ld-linux.so.2 $TOOLCHAIN_ROOT/lib/ld-lsb.so.3
+    ;;
+    x86_64) ln -sfv ../lib/ld-linux-x86-64.so.2 $TOOLCHAIN_ROOT/lib64
+            ln -sfv ../lib/ld-linux-x86-64.so.2 $TOOLCHAIN_ROOT/lib64/ld-lsb-x86-64.so.3     ;;
+esac
+
+mkdir build && cd build
+
+echo "rootsbindir=/usr/sbin" > configparms
+
+../configure                                       \
+    --prefix=$TOOLCHAIN_ROOT                        \
+    --host=$LFS_TGT                                \
+    --build=$(../scripts/config.guess)              \
+    --enable-kernel=3.2                            \
+    --with-headers=$TOOLCHAIN_ROOT/include          \
+    libc_cv_slibdir=/usr/lib &&
+    make -j$(nproc) &&
+    make DESTDIR=$TOOLCHAIN_ROOT/ install
+
+sed '/RTLDLIST=/s@/usr@@g' -i TOOLCHAIN_ROOT/usr/bin/ldd
+
+echo 'int main(){}' | $LFS_TGT-gcc -xc -readelf -l a.out | grep ld-linux
+rm -v a.out
+TOOLCHAIN_ROOT/libexec/gcc/$LFS_TGT/12.2.0/install-tools/mkheaders
+
+cd ../../
+
 
